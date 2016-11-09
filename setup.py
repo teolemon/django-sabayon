a826d1b077ffbada5663585df275c56e1747196c
w0de <harry@sysop.ooo>
2016-11-09 03:08:17 -0500

    .

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name='django-sabayon',
     version='0.1',
     packages=find_packages(),
-    install_requires=['Django>=1.10'],
     author='w0de',
     author_email='harry@sysop.ooo',
     description='Django support for https://github.com/dmathieu/sabayon'
