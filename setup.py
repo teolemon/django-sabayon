272d684a903d67078b27d6c10f9fdecd85f58df7
w0de <harry@sysop.ooo>
2016-11-09 03:09:08 -0500

    0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-sabayon',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     author='w0de',
     author_email='harry@sysop.ooo',
